cb521edfafc105d4890bdcf46535e26aff9e2bce
Aleks <akamenovic@hawk.iit.edu>
2021-03-16 01:28:26 -0500

    Add basic js function for on submit

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,3 +3,13 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+const success = document.getElementById('success');
+
+form.onsubmit = submit;
+
+function submit(event) {
+  success.removeAttribute('hidden');
+
+  event.preventDefault(); //This stops it from actually submitting data.
+}
